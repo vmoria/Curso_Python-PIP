43067fc7c9d7e85145e777ec0a8a05c263bfe05a
vmoria <jorgeandresmorac@gmail.com>
2024-05-20 16:30:02 -0500

    App modified with pandas

--- App/main.py
+++ App/main.py
@@ -1,22 +1,22 @@
-import c_Read_Data
+import c_Read_Data # I can delete this row cuz of line 15
 import d_Charts
 import e_Chart_Data
-# import pandas as Pd
+import pandas as Pd
 
 def run():
-  Data = c_Read_Data.Read_CSV('Data.csv')
-  # SUSPENDIDO POR EL USO DE PANBAS
+  Data = c_Read_Data.Read_CSV('Data.csv') # I need to change c_Read_Data to delete the import of this module
+  '''# SUSPENDIDO POR EL USO DE PANBAS
   Data = list(filter(lambda item: item['Continent'] == 'South America', Data)) # Filter the List Data, and return a List with the country that matches the Country parameter
   
   Countries = list(map(lambda X: X['Country/Territory'], Data))
   Percentages = list(map(lambda X: X['World Population Percentage'], Data))
   '''
   # Df = DataFrame
-  Df = Pd.read_csv('Data.csv') # With this it isn`t necessary c_Read_Data Module
-  Df = Df[Df['Continent'] == 'South America'] # this is the same code as line 8
-  Countries = Df['Country'].values # This is the same code as line 9
-  Percentages = Df['World Population Percentage'].values # This is the same code as line 10
-  '''
+  Df = Pd.read_csv('Data.csv') # With this it isn`t necessary c_Read_Data Module - Transfor the file in Dictionary format
+  Df = Df[Df['Continent'] == 'South America'] # this is the same code as line 9 - This is the filter
+  
+  Countries = Df['Country/Territory'].values # This is the same code as line 11 - Selecting the column
+  Percentages = Df['World Population Percentage'].values # This is the same code as line 12 - Selecting the column
   d_Charts.Generate_Pie_Chart(Countries, Percentages)
 
   Country = input('Type Country => ')
